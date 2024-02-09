ee4f817b82aebcb426a0a99d8e213718400ce093
martonesantana <martone.santana@hotmail.com>
2024-02-09 20:18:25 -0300

    lnw trilha react native

---
+++ src/app/cart.tsx
@@ -0,0 +1,116 @@
+import { View, Text, ScrollView, Alert, Linking } from "react-native";
+import { Header } from "@/components/header";
+import { Product } from "@/components/product";
+import { ProductCartProps, useCartStore } from "@/stores/cart-store";
+import { LinkButton } from "@/components/link-button";
+import { formatCurrency } from "@/utils/functions/format-currency";
+import { Input } from "@/components/input";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
+import { Button } from "@/components/button";
+import { Feather } from "@expo/vector-icons";
+import { useState } from "react";
+import { useNavigation } from "@react-navigation/native";
+
+const PHONE_NUMBER = "5579988076962";
+
+export default function Cart() {
+  const [address, setAddress] = useState("");
+  const cartStore = useCartStore();
+  const navigation = useNavigation();
+
+  const total = formatCurrency(
+    cartStore.products.reduce(
+      (total, product) => total + product.price * product.quantity,
+      0
+    )
+  );
+
+  function handleProductRemove(product: ProductCartProps) {
+    Alert.alert(
+      "Remover produto",
+      `Deseja remover o produto ${product.title}?`,
+      [
+        {
+          text: "Cancelar",
+        },
+        {
+          text: "Remover",
+          onPress: () => cartStore.remove(product.id),
+        },
+      ]
+    );
+  }
+
+  function handleOrder() {
+    if (address.trim().length === 0) {
+      return Alert.alert("Pedido", "Informe o endereço de entrega");
+    }
+    const products = cartStore.products
+      .map((product) => `\n ${product.quantity} ${product.title}`)
+      .join("");
+
+    const message = `
+      \n🍔 NOVO PEDIDO
+      \n🛵 Entregar em: ${address}
+      \n📦 Produtos: ${products}
+      \n💵 Valor Total: ${total}
+      `;
+
+    Linking.openURL(
+      `http://api.whatsapp.com/send?phone=${PHONE_NUMBER}&text=${message}`
+    );
+    cartStore.clear();
+    navigation.goBack();
+  }
+
+  return (
+    <View className="flex-1 pt-8">
+      <Header title="Seu carrinho" />
+      <KeyboardAwareScrollView>
+        <ScrollView>
+          <View className="p-5 flex-1">
+            {cartStore.products.length > 0 ? (
+              <View className="border-b border-slate-700">
+                {cartStore.products.map((product) => (
+                  <Product
+                    key={product.id}
+                    data={product}
+                    onPress={() => handleProductRemove(product)}
+                  />
+                ))}
+              </View>
+            ) : (
+              <Text className="font-body text-slate-400 text-center my-8">
+                Seu carrinho está vazio! 😢
+              </Text>
+            )}
+
+            <View className="flex-row gap-2 items-center mt-5 mb-4">
+              <Text className="text-white text-xl font-subtitle">Total:</Text>
+              <Text className="font-heading text-purple-300 text-2xl">
+                {total}
+              </Text>
+            </View>
+            <Input
+              placeholder="Informe o endereço de entrega com rua, bairro, CEP, número e complemento.."
+              onChangeText={setAddress}
+              blurOnSubmit={true}
+              onSubmitEditing={handleOrder}
+              returnKeyType="next"
+            />
+          </View>
+        </ScrollView>
+      </KeyboardAwareScrollView>
+
+      <View className="p-5 gap-5">
+        <Button onPress={handleOrder}>
+          <Button.Text>Enviar Pedido</Button.Text>
+          <Button.Icon>
+            <Feather name="arrow-right-circle" size={20} />
+          </Button.Icon>
+        </Button>
+        <LinkButton title="Voltar ao cardápio" href="/" />
+      </View>
+    </View>
+  );
+}
---
+++ src/components/loading.tsx
@@ -0,0 +1,10 @@
+import colors from "tailwindcss/colors";
+import { ActivityIndicator, View } from "react-native";
+
+export function Loading() {
+  return (
+    <View className="flex-1 items-center justify-center bg-slate-900">
+      <ActivityIndicator color={colors.white} />
+    </View>
+  );
+}
